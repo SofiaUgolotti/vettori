d6318e95dc5e7981db74af08dcd5a23c4b602eb6
Utente <Utente@LAPTOP-SOFIA>
2024-02-19 16:24:42 +0100

    Aggiungere i file di progetto.

---
+++ vettori/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vettori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(3, 4);
+            Vector v2= new Vector(5, 6);
+            
+            Vector somma =v1+v2;
+            Console.WriteLine(somma);
+            Vector sottrazione =v1-v2;
+            Console.WriteLine(sottrazione);
+            Vector prodotto =v1*2;
+            Console.WriteLine(prodotto);
+            Vector prodottoo=2*v1;
+            Console.WriteLine(prodottoo);
+            Vector diviso = v1 / 2;
+            Console.WriteLine(diviso);
+            Vector positivo = +v1;
+            Console.WriteLine(positivo);
+            Vector negativo = -v1;
+            Console.WriteLine(negativo);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ vettori/Vector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vettori
+{
+    internal class Vector
+    {
+        public readonly double X;
+        public readonly double Y;
+        public Vector(double x, double y)
+        {  X = x; Y = y; }
+        public static Vector operator +(Vector v1, Vector v2) 
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+        public static Vector operator -(Vector v1, Vector v2) 
+        { 
+            return new Vector(v1.X-v2.X, v1.Y-v2.Y);
+        }
+        public static Vector operator *(Vector v1,double scalare) 
+        {
+            return new Vector(v1.X*scalare, v1.Y*scalare);
+        }
+        public static Vector operator *(double scalare,Vector v1)
+        {
+            return new Vector(scalare * v1.X, scalare * v1.Y);
+        }
+        public static Vector operator /(Vector v1,double scalare)
+        {
+            return new Vector(v1.X / scalare, v1.Y / scalare);
+        }
+        public static Vector operator +(Vector v1)
+        {
+            return +v1;
+        }
+        public static Vector operator -(Vector v1)
+        {
+            return -v1;
+        }
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+}
